f844fce6f5a6de7d5ce105b02249bb26664e7cd4
Maarten Dohmen <mdohmen.contractor@libertyglobal.com>
2023-02-15 14:29:22 +0100

    Release 0.0.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Release 0.0.4");
+        System.out.println("Release 0.0.5");
     }
 
     private void feature1() {
