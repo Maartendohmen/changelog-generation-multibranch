6e71bd38477b02e6933b67d802cc5b8e14371797
Dennis Vermeulen <dvermeulen.contractor@libertyglobal.com>
2023-02-15 13:27:14 +0100

    Changed added feature 8 initial to 10.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -55,7 +55,7 @@ public class Main {
     }
 
     private void feature10() {
-        System.out.println("added feature 8 initial");
+        System.out.println("added feature 10 initial");
         System.out.println("added feature 8 expension");
         System.out.println("finished feature 8");
     }
