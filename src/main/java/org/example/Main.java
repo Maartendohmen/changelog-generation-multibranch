901847d5a9e2ca56fc3bd71e97728c201220541a
Maarten Dohmen <mdohmen.contractor@libertyglobal.com>
2023-02-15 10:19:28 +0100

    FEATURE-1 Moved feature 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
+    }
+
+    private void feature1() {
         System.out.println("added feature 1 initial");
         System.out.println("added feature 1 expension");
+        System.out.println("moved feature 1");
     }
 }
