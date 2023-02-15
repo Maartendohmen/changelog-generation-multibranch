a8171f033f859669b07d5003ad24fea660c18323
Dennis Vermeulen <dvermeulen.contractor@libertyglobal.com>
2023-02-15 13:26:55 +0100

    FEATURE-10. Created feature10.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -53,4 +53,10 @@ public class Main {
         System.out.println("added feature 8 expension");
         System.out.println("finished feature 8");
     }
+
+    private void feature10() {
+        System.out.println("added feature 8 initial");
+        System.out.println("added feature 8 expension");
+        System.out.println("finished feature 8");
+    }
 }
