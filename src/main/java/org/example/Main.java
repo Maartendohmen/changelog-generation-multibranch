f46fe1f56dabfe53a8dfd96a0792f270ca914db6
Maarten Dohmen <mdohmen.contractor@libertyglobal.com>
2023-02-15 10:15:04 +0100

    FEATURE-1 Added initial setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Master branch created");
+        System.out.println("added feature 1 initial");
     }
 }
