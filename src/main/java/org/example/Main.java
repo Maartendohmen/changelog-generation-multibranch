21a5dd1cad4e4a562123c1c1a6e012912f1cf835
Maarten Dohmen <mdohmen.contractor@libertyglobal.com>
2023-02-15 13:14:07 +0100

    FEATURE-7 Added initial setup

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -41,4 +41,8 @@ public class Main {
         System.out.println("added feature 6 expension");
         System.out.println("finished feature 6");
     }
+
+    private void feature7() {
+        System.out.println("added feature 7 initial");
+    }
 }
