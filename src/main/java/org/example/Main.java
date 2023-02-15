51f3fb2a8f088bad768b2e5270c3280b6b5c32a4
Dennis Vermeulen <dvermeulen.contractor@libertyglobal.com>
2023-02-15 13:27:35 +0100

    Changed added feature 8 expension to 10.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -56,7 +56,7 @@ public class Main {
 
     private void feature10() {
         System.out.println("added feature 10 initial");
-        System.out.println("added feature 8 expension");
+        System.out.println("added feature 10 expension");
         System.out.println("finished feature 8");
     }
 }
